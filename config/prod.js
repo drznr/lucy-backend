8d85a64587221db42becc0d33b9026d97e23f6ef
drznr <44776027+drznr@users.noreply.github.com>
2020-03-28 21:44:51 +0300

    front end added

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "dbURL": "mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority",
+  "dbURL": "mongodb+srv://drznr:<password>@cluster0-oeron.mongodb.net/test?retryWrites=true&w=majority",
 }
---
+++ public/precache-manifest.ad824e2d95c81a6cc1dbd2eeaa6792a6.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ee6c44e04a1c07e6fe",
+    "url": "/css/app.81cad688.css"
+  },
+  {
+    "revision": "e12936e433c2af9697f0",
+    "url": "/css/chunk-vendors.4ecd944a.css"
+  },
+  {
+    "revision": "32f0422680d63e98150d3a17a74b1e4d",
+    "url": "/fonts/BalooThambi2-Regular.32f04226.ttf"
+  },
+  {
+    "revision": "30547aae3d169616919d266027b39c44",
+    "url": "/fonts/DancingScript-Medium.30547aae.ttf"
+  },
+  {
+    "revision": "411051cb61bcda5517943601e8734cea",
+    "url": "/fonts/Monoton-Regular.411051cb.ttf"
+  },
+  {
+    "revision": "dad7c1981f86bd835b0f6d29865f0653",
+    "url": "/fonts/OdibeeSans-Regular.dad7c198.ttf"
+  },
+  {
+    "revision": "17a1468e62195d0688a6f3bd12da2e92",
+    "url": "/fonts/Overpass-SemiBold.17a1468e.ttf"
+  },
+  {
+    "revision": "c9fffa0eb79167d1f135114691d6a9a3",
+    "url": "/img/account.c9fffa0e.svg"
+  },
+  {
+    "revision": "25c546c84f63cd12d8d163158ac9eef9",
+    "url": "/img/app-bg-default.25c546c8.jpg"
+  },
+  {
+    "revision": "a903bc83eebbbb37cfc2046149dff1f9",
+    "url": "/img/chapman.a903bc83.jpg"
+  },
+  {
+    "revision": "0bf611ea48ab16bb2825f98b7e98e0fa",
+    "url": "/img/chat.0bf611ea.svg"
+  },
+  {
+    "revision": "fb8958f7b49bd2e09220215a3ce94770",
+    "url": "/img/claps.fb8958f7.svg"
+  },
+  {
+    "revision": "6eba11b738545b149c802a8f22bd4750",
+    "url": "/img/crowd.6eba11b7.jpg"
+  },
+  {
+    "revision": "f037640c6e0f17655c46f89ed215978a",
+    "url": "/img/default.f037640c.jpg"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/img/edit.2927f8de.svg"
+  },
+  {
+    "revision": "68a65e389e6d51d3e5b5e8eb63fca6cb",
+    "url": "/img/home-bg-default.68a65e38.jpg"
+  },
+  {
+    "revision": "b12643a36188d7950fa078cfbfd19a24",
+    "url": "/img/jukebox.b12643a3.svg"
+  },
+  {
+    "revision": "e6d9f9ebaa3bd81efa96945039e6ef66",
+    "url": "/img/jukebox.e6d9f9eb.png"
+  },
+  {
+    "revision": "f868945d423321c60f6fabe2ddbe3945",
+    "url": "/img/music-note.f868945d.svg"
+  },
+  {
+    "revision": "877605a7119ef31522aa8d0adc4f3a95",
+    "url": "/img/player.877605a7.png"
+  },
+  {
+    "revision": "02084376eff5d82d588aadc59a813fa9",
+    "url": "/img/playlist.02084376.svg"
+  },
+  {
+    "revision": "867418af3746079ec132b7557d9d8372",
+    "url": "/img/plus.867418af.svg"
+  },
+  {
+    "revision": "ecef733a33ceec8dc32d29510998afe9",
+    "url": "/img/search.ecef733a.svg"
+  },
+  {
+    "revision": "fc920acdd1334e60ae9f914cd37d1d04",
+    "url": "/img/station-default-thumb.fc920acd.png"
+  },
+  {
+    "revision": "f7566294a66ec00b9ada0d33c7e06602",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3ee6c44e04a1c07e6fe",
+    "url": "/js/app.2a02a2fe.js"
+  },
+  {
+    "revision": "e12936e433c2af9697f0",
+    "url": "/js/chunk-vendors.794f37c8.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
